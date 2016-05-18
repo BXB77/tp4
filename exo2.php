35dc20d7d738cde49d8e04c34e5c4cfd75e5672d
benjamindecastro <benjamindecastro@hotmail.fr>
2016-05-18 05:50:36 -0400

    insert tout sauf l'idMes

--- exo2.php
+++ exo2.php
@@ -17,43 +17,43 @@
     <br>
     <label>Genre</label>
       <?php
-      echo "<select name="."genre".">";
-      $resultat=mysqli_query($link,"select distinct genre from Film");
-     if($resultat)
-     {
-       while ($film=mysqli_fetch_assoc($resultat)){
-        echo "<option>".$film['genre']."</option>";
-       }
-      echo "</select>";
-     }
-    ?>
+        echo "<select name="."genre".">";
+        $resultat=mysqli_query($link,"select distinct genre from Film");
+        if($resultat)
+        {
+          while ($film=mysqli_fetch_assoc($resultat)){
+            echo "<option>".$film['genre']."</option>";
+          }
+        echo "</select>";
+        }
+      ?>
     <br>
     <label>Pays</label>
     <?php
       echo "<select name="."pays".">";
       $resultat=mysqli_query($link,"select distinct codePays from Film");
-     if($resultat)
-     {
-       while ($film=mysqli_fetch_assoc($resultat)){
-        echo "<option>".$film['codePays']."</option>";
-       }
+      if($resultat)
+      {
+        while ($film=mysqli_fetch_assoc($resultat)){
+          echo "<option>".$film['codePays']."</option>";
+        }
       echo "</select>";
-     }
+      }
     ?>
     <br>
     <label>Realisateur</label>
     <?php
       echo "<select name="."realisateur".">";
       $resultat=mysqli_query($link,"select distinct nom from Film join Artiste where idArtiste=idMes");
-     if($resultat)
-     {
-       echo "<option>"."Tous"."</option>";
-       while ($film=mysqli_fetch_assoc($resultat)){
-        echo "<option>".$film['nom']."</option>";
-       }
-    echo "</select>";
-     }
-      ?>
+      if($resultat)
+      {
+        echo "<option>"."Tous"."</option>";
+        while ($film=mysqli_fetch_assoc($resultat)){
+          echo "<option>".$film['nom']."</option>";
+        }
+        echo "</select>";
+      }
+    ?>
     <br>
     <label>Resume</label>
     <textarea rows="3" cols="30" name="resume"></textarea>
@@ -64,32 +64,36 @@
     <input type="submit" value="Ajoutez"/></input>
 </form>
     <table border=1>
-      <tr>
+       <tr>
         <th>Titre</th>
         <th>Genre</th>
       </tr>
-   <?php
-      if(isset($_GET["nom"]) && isset($_GET["genre"]) && isset($_GET["pays"]) && isset($_GET["realisateur"]) && isset($_GET["resume"]) && isset($_GET["annee"])){
-      $nom=$_GET["nom"];
-      $genre=$_GET["genre"];
-      $annee=$_GET["annee"];
-      $sql="INSERT INTO Artiste(nom,prenom,anneeNaiss) VALUES ('".$nom."','".$prenom."','".$annee."')";
-      $requete=mysqli_query($link,$sql);
-      }
-       $resultat=mysqli_query($link,"select titre,genre from Film");
-       if($resultat)
-       {
-        while ($film=mysqli_fetch_assoc($resultat)){
-            echo "<tr>";
-            echo "<td>".$film['titre']."</td>";
-            echo "<td>".$film['genre']."</td>";
-            echo "</tr>";
+      <?php
+        if(isset($_GET["nom"]) && isset($_GET["genre"]) && isset($_GET["pays"]) && isset($_GET["realisateur"]) && isset($_GET["resume"]) && isset($_GET["annee"])){
+        $nom=$_GET["nom"];
+        $genre=$_GET["genre"];
+        $pays=$_GET["pays"];
+        $realisateur=$_GET["realisateur"];
+        $resume=$_GET["resume"];
+        $annee=$_GET["annee"];
+        $idMes=mysqli_query($link,"select idMes from Film F join Artiste A where idArtiste=idMes and A.nom=".$realisateur);
+        $sql="INSERT INTO Film(titre,genre,codePays,idMes,resume,annee) VALUES ('".$nom."','".$genre."','".$pays."','".$idMes."','".$resume."','".$annee."')";
+        $requete=mysqli_query($link,$sql);
         }
-       }
-       else
-         die("<p>Erreur dans l'execution de la requete.</p>");
-       mysqli_close($link);
-   ?>
+         $resultat=mysqli_query($link,"select titre,genre from Film");
+         if($resultat)
+         {
+          while ($film=mysqli_fetch_assoc($resultat)){
+              echo "<tr>";
+              echo "<td>".$film['titre']."</td>";
+              echo "<td>".$film['genre']."</td>";
+              echo "</tr>";
+          }
+         }
+         else
+           die("<p>Erreur dans l'execution de la requete.</p>");
+         mysqli_close($link);
+     ?>
     </table>
   </body>
 </html>
